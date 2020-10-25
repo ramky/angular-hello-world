44f1d3ca6dacb43f91e756f25d5630aa170b2545
Ram Iyer <ramky.iyer@gmail.com>
2020-10-24 22:33:42 -0400

    fixing js specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-hello-world app is running!');
+    console.log("******compiled ", compiled);
+    expect(compiled.querySelector('h1').textContent).toContain('angular-hello-world');
   });
 });
